6d4063aca17a27cddf578bd918860cf28986b4a3
Madhumithaakathiravansoundari <madhumithaakathiravan07@gmail.com>
2026-08-31 21:37:49 +0530

    Add environment verification script

---
+++ src/test_environment.py
@@ -0,0 +1,8 @@
+import tensorflow as tf
+import numpy as np
+import cv2
+
+print("TensorFlow version:", tf.__version__)
+print("NumPy version:", np.__version__)
+print("OpenCV version:", cv2.__version__)
+print("Environment setup successful!")
