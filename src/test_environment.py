d9ff9e56b66a6a063d3922ad7f6dc3a1613a2e61
charugethci <charugethci@gmail.com>
2026-09-03 18:57:17 +0530

    Add final experiment results and clean project files

--- src/test_environment.py
+++
@@ -1,8 +0,0 @@
-import tensorflow as tf
-import numpy as np
-import cv2
-
-print("TensorFlow version:", tf.__version__)
-print("NumPy version:", np.__version__)
-print("OpenCV version:", cv2.__version__)
-print("Environment setup successful!")
